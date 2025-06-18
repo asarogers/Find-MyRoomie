56563ea972dbf8a73955fe9a303fea74a8a6ba4a
asarogers <cyberasasoftware@gmail.com>
2025-06-18 12:47:59 -0500

    adding sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.yoursite.com',
+    generateRobotsTxt: true, // (optional)
+    sitemapSize: 7000,
+  };
+  

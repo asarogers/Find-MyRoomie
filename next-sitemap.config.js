b0804065fabeed191ebee00113083bdceed5796b
asarogers <cyberasasoftware@gmail.com>
2025-06-20 17:11:22 -0500

    adding map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
+const cityList = require('./src/components/_data/cities.json');
+
 module.exports = {
-    siteUrl: 'https://findmyroomy.com', // ✅ updated domain
-    generateRobotsTxt: true,
-    generateIndexSitemap: true,
-    sitemapSize: 5000,
-    outDir: './out',
-  }
-  
+  siteUrl: 'https://findmyroomy.com',
+  generateRobotsTxt: true,
+  exclude: ['/api/*'],
+
+  // Inject custom dynamic routes
+  additionalPaths: async (config) => {
+    const paths = [];
+
+    for (const city of cityList) {
+      paths.push({ loc: `/apartments/${city.slug}` });
+      paths.push({ loc: `/roommates/${city.slug}` });
+      paths.push({ loc: `/pets/${city.slug}` });
+    }
+
+    return paths;
+  },
+};
